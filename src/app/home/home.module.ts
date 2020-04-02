e04f921d5db5f30cc2c6db14e8822f66b34233cc
Carlos Rojas <carlos.rojas@localhost>
2020-04-01 20:02:15 -0500

    upgrading angular done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeRoutingModule} from './home-routing.module';
-import {SharedModule} from "../shared/shared.module";
+import {SharedModule} from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { SectionTopoComponent } from './section-topo/section-topo.component';
 import { FormInscricaoComponent } from './form-inscricao/form-inscricao.component';
-import { ContadorComponent } from './contador/contador.component';
-import { ValoresComponent } from './contador/valores/valores.component';
 import { SectionPalestrantesComponent } from './section-palestrantes/section-palestrantes.component';
 import { TituloComponent } from './titulo/titulo.component';
 import { PalestranteComponent } from './section-palestrantes/palestrante/palestrante.component';
@@ -22,6 +20,18 @@ import { SectionMatrizComponent } from './section-matriz/section-matriz.componen
         SharedModule,
         HomeRoutingModule
     ],
-    declarations: [HomeComponent, SectionTopoComponent, FormInscricaoComponent, ContadorComponent, ValoresComponent, SectionPalestrantesComponent, TituloComponent, PalestranteComponent, SectionAgendaComponent, AgendaComponent, SectionMapaComponent, SectionApoioComponent, SectionMatrizComponent]
+    declarations: [
+        HomeComponent,
+        SectionTopoComponent,
+        FormInscricaoComponent,
+        SectionPalestrantesComponent,
+        TituloComponent,
+        PalestranteComponent,
+        SectionAgendaComponent,
+        AgendaComponent,
+        SectionMapaComponent,
+        SectionApoioComponent,
+        SectionMatrizComponent
+    ]
 })
 export class HomeModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {SharedRoutingModule} from './shared-routing.module';
 
-import {MaterializeModule} from "angular2-materialize";
+import {MaterializeModule} from 'angular2-materialize';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        HttpModule,
         MaterializeModule,
         SharedRoutingModule
     ],
@@ -20,7 +18,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
     ],
     exports: [
         FormsModule,
-        HttpModule,
         MaterializeModule,
         NotFoundComponent
     ]
