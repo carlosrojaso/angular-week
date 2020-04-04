769f93291ee11f51ea56df1f7f5f018978ad2123
Carlos Rojas <carlos.rojas@localhost>
2020-04-04 17:03:13 -0500

    updating speakers

---
+++ src/app/config.ts
@@ -0,0 +1,316 @@
+export const agenda =
+[
+    {
+        dia: 'Miercoles',
+        hora: '07:00pm - 07:30pm',
+        nome: 'Oscar Barajas',
+        titulo: '¿Qué es Svelte.js?',
+        desc: `Svelte un nuevo framework de JavaScript que nos permite construir interfaces de
+        usuario. Es considerado el nuevo competidor en la batalla de los frontends donde compiten
+        React, Angular o Vue.`,
+        imagem: '../../../../assets/img/oscar-barajas.png'
+    },
+    {
+        dia: 'Miercoles',
+        hora: '07:30pm - 8:00pm',
+        nome: 'Julián Duque',
+        titulo: 'Aprendamos sobre Debugging en Node.js',
+        desc: `En esta charla vamos a explorar las diferentes herramientas y técnicas para
+        depurar aplicaciones JavaScript y Node.js`,
+        imagem: '../../../../assets/img/julian-duque.jpg'
+    },
+    {
+        dia: 'Miercoles',
+        hora: '08:00pm - 8:30pm',
+        nome: 'Yeison Daza',
+        titulo: 'Design token en CSS',
+        desc: `Voy a hablar sobre la evolución en los enfoques para escribir css donde uno de los
+        mayores restos a sido mantener la consistencia, hasta llegar a hablar sobre design tokens`,
+        imagem: '../../../../assets/img/yeison-daza.jpg'
+    },
+    {
+        dia: 'Miercoles',
+        hora: '08:30pm - 9:00pm',
+        nome: 'Jorge Cano',
+        titulo: 'Reactive Man: la verdadera reactividad',
+        desc: `Para ser un maestro pokemon tenemos que atraparlos a todos, para ser una maestro en RxJS tenemos que llamar a reactive man,
+        el nos ayudara a entender nuestros poderes reactivos!`,
+        imagem: '../../../../assets/img/jorge-cano.jpg'
+    },
+    {
+        dia: 'Miercoles',
+        hora: '09:00pm - 9:30pm',
+        nome: 'Maria Alejandra Giraldo',
+        titulo: 'NestJS, Como sacarle jugo al back-end siendo front-end',
+        desc: 'En en esta charla vamos a aprender como los conocimientos de front-end se pueden fusionar en el back-end',
+        imagem: '../../../../assets/img/maleja-giraldo.jpg'
+    },
+    {
+        dia: 'Jueves',
+        hora: '07:00pm - 7:30pm',
+        nome: 'Nicolas Molina Monroy',
+        titulo: 'Acelera la velocidad de navegación de tu Aplicación Angular',
+        desc: 'En esta charla vas a aprender a como acelerar la experiencia de navegación de tus usuarios en aplicaciones Angular.',
+        imagem: '../../../../assets/img/nicolas-monroy.jpg'
+    },
+    {
+        dia: 'Jueves',
+        hora: '07:30pm - 8:00pm',
+        nome: 'Sergio Valencia',
+        titulo: 'Performance en JS',
+        desc: 'Aprende sobre buenas prácticas que te ayudará a mejorar el performance de tu codigo',
+        imagem: '../../../../assets/img/sergio-valencia.jpg'
+    },
+    {
+        dia: 'Jueves',
+        hora: '08:00pm - 8:30pm',
+        nome: 'Fabio Cortés',
+        titulo: 'WebXR: La nueva generación de experiencias inmersivas en la web',
+        desc: `Cuando hablamos de experiencias inmerivas en la web nos referimos a todo el espectro de aplicaciones
+        que van desde el mundo real hasta la realidad virtual`,
+        imagem: '../../../../assets/img/fabio-cortes.jpg'
+    },
+    {
+        dia: 'Jueves',
+        hora: '08:30pm - 9:00pm',
+        nome: 'Samuel Burbano',
+        titulo: 'Vue 3.0 - Composition API',
+        desc: `Veamos la nueva Composition API propuesta para la próxima versión de Vue, como ayuda a mejorar el desarrollo
+        en Vue y la polémica que ha generado en los desarrolladores que usan y aman Vue`,
+        imagem: '../../../../assets/img/samuel-burbano.jpg'
+    },
+    {
+        dia: 'Miercoles',
+        hora: '09:00pm - 9:30pm',
+        nome: 'Mayra Rodriguez',
+        titulo: 'Promesas vs Observable',
+        desc: `¿Cuándo usar promesas? ¿Cuándo usar Observables? haremos una comparación entre ambos y daremos ejemplos
+        reales de cuando hacer desarrollos imperativos o reactivos para que subas tu nivel`,
+        imagem: '../../../../assets/img/mayra-rodriguez.png'
+    },
+    {
+        dia: 'Viernes',
+        hora: '07:00 pm - 7:00pm',
+        nome: 'Sebastian Gomez',
+        titulo: 'Manejando queues con Bull.js',
+        desc: 'Aprende a manejar facilmente queues y concurrencia con bull.js',
+        imagem: '../../../assets/img/sebastian.jpg'
+    },
+    {
+        dia: 'Viernes',
+        hora: '07:30pm - 8:00pm',
+        nome: 'Ricardo Chavarria',
+        titulo: 'WebPack, ¿Cómo aprovecharlo al máximo?',
+        desc: `En está pequeña charla daremos los primeros pasos con Webpack para conocer sus beneficios y como aprovechalos al máximo,
+        veremos cual es la necesidad de usarlo y tocaremos temas como Hot Reload, Minimización automática,
+        Babel y otros temas interesantes de Webpack.`,
+        imagem: '../../../assets/img/ricardo-chavarria.jpg'
+    },
+    {
+        dia: 'Viernes',
+        hora: '08:00pm - 8:30pm',
+        nome: 'Paula Mazo',
+        titulo: 'Docker para Front-ends',
+        desc: 'Que cosas debes tener en cuenta para usar Docker con tu proyecto en JS',
+        imagem: '../../../assets/img/paula-mazo.jpg'
+    },
+    {
+        dia     : 'Viernes',
+        hora    : '8:30pm - 9:00pm',
+        nome    : 'Vanessa Aristizabal',
+        titulo  : 'Meeseeks de JavaScript',
+        desc    : `Los Meeseeks , son criaturas que sirven a un propósito en particular y hay algunas características de JavaScript
+        que podríamos llamar Meeseeks, que nos facilitan mucho la creación de nuestros código y en esta charla quiero darles a conocer
+        algunos de ellos.`,
+        imagem: '../../../assets/img/vanessa.jpg'
+    },
+    {
+        dia: 'Viernes',
+        hora: '09:00pm - 9:30pm',
+        nome: 'Jesús Salvador Rodríguez Cardiel',
+        titulo: 'JS ha cambiado mi vida',
+        desc: 'Como JS revolucionó mi forma de trabajo',
+        imagem: '../../../assets/img/jesus-salvador.jpg'
+    }
+];
+
+export const speakers = [
+    {
+        nome: 'Oscar',
+        sobrenome: 'Barajas',
+        imagem: '../../../../assets/img/oscar-barajas.png',
+        redes: {
+            twitter : 'https://twitter.com/gndx'
+        },
+        bio: `Frontend & Foundation Layer at @platzi #education - Lead at Developer Circles from Facebook, ReactJS,
+        Speaker & Blogger. I teach ReactJS & Svelte in @platzi`,
+    },
+
+    {
+        nome: 'Julián',
+        sobrenome: 'Duque',
+        imagem: '../../../../assets/img/julian-duque.jpg',
+        redes: {
+            twitter : 'https://twitter.com/julian_duque'
+        },
+        bio: `Desarrollador y Educador en MNTD, Developer Advocate en Salesforce Heroku, Organizador
+        de NodeConf y JSConf Colombia.`,
+    },
+
+    {
+        nome: 'Yeison',
+        sobrenome: 'Daza',
+        imagem: '../../../../assets/img/yeison-daza.jpg',
+        redes: {
+            twitter : 'https://twitter.com/Yeion7'
+        },
+        bio: `Frontend developer`,
+    },
+
+    {
+        nome: 'Jorge',
+        sobrenome: 'Cano',
+        imagem: '../../../../assets/img/jorge-cano.jpg',
+        redes: {
+            twitter : 'https://twitter.com/jorgeucano'
+        },
+        bio: `Después de más de 10 años trabajando en la industria de la tecnología, Jorge es un full stack developer con
+        enfoque en tecnologías de JavaScript. Hoy está trabajando en HeroDevs como Arquitecto principal, enfocado en Scully
+        al 100% de su dia. Entregó varios cursos y charlas, y también escribe artículos técnicos sobre estos temas.
+        Debido a eso, fue reconocido como un Google Developer Expert en Angular y tecnologías Web, como asi telerik le otorgo
+        el Nativescript Developer Expert`,
+    },
+
+    {
+        nome: 'Maria Alejandra',
+        sobrenome: 'Giraldo',
+        imagem: '../../../../assets/img/maleja-giraldo.jpg',
+        redes: {
+            twitter : 'https://twitter.com/maleja111'
+        },
+        bio: `Computer engineer, +7 years as a web developer. Organizer of  @SheCodesAngular  & Co-Organizer of  @NodeConfCo
+        - Fullstack developer at  @BunnyStudi0`,
+    },
+
+    {
+        nome: 'Nicolas',
+        sobrenome: 'Molina',
+        imagem: '../../../../assets/img/nicolas-monroy.jpg',
+        redes: {
+            twitter : 'https://twitter.com/nicobytes'
+        },
+        bio: `I am a GDE (Google Developer Expert) in Web Technologies and Angular. I love to be a developer and to create software.
+        Currently, I'm a Tech leader and work mainly in single page applications with Angular and Ionic for frontend,
+        NodeJs /Python for web services with GraphQL.`,
+    },
+
+    {
+        nome: 'Sergio',
+        sobrenome: 'Valencia',
+        imagem: '../../../../assets/img/sergio-valencia.jpg',
+        redes: {
+            twitter : 'https://twitter.com/sergio91044'
+        },
+        bio: 'Desarrollador Front End con 4 años de experiencia usando Angular y React, fanático de los libros y las nuevas tecnológicas.',
+    },
+
+    {
+        nome: 'Fabio',
+        sobrenome: 'Cortés',
+        imagem: '../../../../assets/img/fabio-cortes.jpg',
+        redes: {
+            twitter : 'https://twitter.com/fabiojcortes'
+        },
+        bio: `WebXR developer en EPFL. Apasionado por la intersección entre arte y la tecnología.`,
+    },
+
+    {
+        nome: 'Samuel',
+        sobrenome: 'Burbano',
+        imagem: '../../../../assets/img/samuel-burbano.jpg',
+        redes       : {
+            twitter : 'https://twitter.com/iosamuel'
+        },
+        bio: 'Programador de software, entusiasta de la web. Senior Frontend developer en Ring (Amazon).',
+    },
+
+    {
+        nome: 'Mayra',
+        sobrenome: 'Rodriguez',
+        imagem: '../../../../assets/img/mayra-rodriguez.png',
+        redes: {
+            twitter : 'https://twitter.com/mayrascript',
+        },
+        bio: `Mom, Developer and Speaker / @GoogleDevExpert  at  @angular  / @WomenTechmakers Ambassador
+        / Computer Engineer / Flutter Colombia / Founder of  @rovistack`,
+    },
+
+    {
+        nome: 'Sebastian',
+        sobrenome: 'Gomez',
+        imagem: '../../../../assets/img/sebastian.jpg',
+        redes: {
+            twitter : 'https://twitter.com/sebasgojs'
+        },
+        bio         : `Mi nombre es Sebastián Gómez, soy Google Developer Expert in Web Technologies,
+        estudié ingeniería de sistemas e Informática y una maestría en Ingeniería de Sistemas de la Universidad Nacional
+        de Colombia. Actualmente trabajo en Globant como arquitecto de Web UI con énfasis en aplicaciones híbridas y
+        cross compiladas. Soy el organizador del Google Developers Group de Medellín, así que contactame si quieres dar
+        alguna charla o participar actuamente de esta comunidad.`
+    },
+
+    {
+        nome: 'Ricardo',
+        sobrenome: 'Chavarria',
+        imagem: '../../../assets/img/ricardo-chavarria.jpg',
+        redes: {
+            twitter : 'https://twitter.com/ricardo_chl',
+        },
+        bio: `Desarrollador de Software orientado a tecnologías de JavaScript, inicio en el mundo de la programación hace más
+        de 6 años, actualmente cuenta más de 3 años trabajando con tecnologías Web y Angular. Hoy en día trabaja como
+        Software Developer en Creative Information Technologies utilizando Angular, Ionic, RxJS, Flutter, Firebase y otras
+        tecnologías de punta… Organizador de la comunidad de Angular en Honduras donde ha impartido múltiples charlas y cursos
+        relacionados con Angular y Tecnologías Web. Organizador de la NG Honduras 2020 (La primera conferencia de Angular y
+        Desarrollo Web en Honduras). También Forma parte del programa Auth0 Ambassador y Git Kraken Ambassador.`,
+    },
+
+    {
+        nome        : 'Paula',
+        sobrenome   : 'Mazo Cortés',
+        imagem      : 'https://avatars2.githubusercontent.com/u/22626905?s=460&v=4',
+        redes       : {
+            twitter : 'https://twitter.com/Mazoster'
+        },
+        bio         : `Conocida como "Mazo" desde el colegio. Ha trabajado en las áreas de Infraestructura, DevOps y
+        en los últimos dos años en desarrollo 💛. Web UI Developer en Globant, amante de Javascript y el clean code.
+        Si le queda tiempo en el sprint :v`,
+    },
+
+    {
+        nome: 'Vanessa',
+        sobrenome: 'Aristizabal',
+        imagem: '../../../assets/img/vanessa.jpg',
+        redes: {
+            twitter : 'https://twitter.com/vanessamarely'
+        },
+        bio: `Vanessa Aristizabal es Ingeniera de sistemas, trabaja como Web UI en Globant. Organizadora de
+        GDG Medellín y colabora en la organización de  NG-Conf Colombia. GDE en Angular & Web Technologies.
+        Apasionada de la tecnología,  el diseño y el Anime.`,
+    },
+
+    {
+        nome        : 'Jesús',
+        sobrenome   : 'Rodríguez',
+        imagem      : '../../../assets/img/jesus-salvador.jpg',
+        redes       : {
+            twitter : 'https://twitter.com/GsusRodriguezC'
+        },
+        bio         : `39 años de Edad. Actualmente trabajo como Profesor de Universidad, impartiendo algunas de las
+        siguientes Materias -> Administración de Proyectos de TI, Administración de Bases de Datos, Desarrollo de Aplicaciones
+        Móviles, Desarrollo de Aplicaciones Web, Estadística, Base de datos para Aplicaciones, Desarrollo de Aplicaciones para
+        la Industria 4.0. Ademas de trabajar como profesor universitario, También soy servidor publico del Gobierno del
+        Estado de Zacatecas, desempeñandome como administrador de Tecnologías, desarrollo de aplicaciones web y enlace de
+        TI con las autoridades Fiscales. Como Independiente he desarrollado proyectos para el ambito privado y Gubernamental.
+        Por el momento estamos en reestructuración del sitio web empresarial: http://www.backgroundlab.com/`,
+    }
+];
--- src/app/home/section-agenda/section-agenda.component.ts
+++ src/app/home/section-agenda/section-agenda.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { agenda as configAgenda } from '../../config';
 
 @Component({
     selector: 'app-section-agenda',
@@ -12,128 +13,7 @@ export class SectionAgendaComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        this.agendas = [
-            {
-                dia: 'Miercoles',
-                hora: '07:00pm - 07:30pm',
-                nome: 'Oscar Barajas',
-                titulo: '¿Qué es Svelte.js?',
-                desc: 'Svelte un nuevo framework de JavaScript que nos permite construir interfaces de usuario. Es considerado el nuevo competidor en la batalla de los frontends donde compiten React, Angular o Vue.',
-                imagem: '../../../../assets/img/oscar-barajas.png'
-            },
-            {
-                dia: 'Miercoles',
-                hora: '07:30pm - 8:00pm',
-                nome: 'Julián Duque',
-                titulo: 'Aprendamos sobre Debugging en Node.js',
-                desc: "En esta charla vamos a explorar las diferentes herramientas y técnicas para depurar aplicaciones JavaScript y Node.js",
-                imagem: '../../../../assets/img/julian-duque.jpg'
-            },
-            {
-                dia: 'Miercoles',
-                hora: '08:00pm - 8:30pm',
-                nome: 'Yeison Daza',
-                titulo: 'Design token en CSS',
-                desc: 'Voy a hablar sobre la evolución en los enfoques para escribir css donde uno de los mayores restos a sido mantener la consistencia, hasta llegar a hablar sobre design tokens',
-                imagem: '../../../../assets/img/yeison-daza.jpg'
-            },
-            {
-                dia: 'Miercoles',
-                hora: '08:30pm - 9:00pm',
-                nome: 'Jorge Cano',
-                titulo: 'Tbd ',
-                desc: 'Tbd ',
-                imagem: '../../../../assets/img/jorge-cano.jpg'
-            },
-            {
-                dia: 'Miercoles',
-                hora: '09:00pm - 9:30pm',
-                nome: 'Maria Alejandra Giraldo',
-                titulo: 'NestJS, Como sacarle jugo al back-end siendo front-end',
-                desc: 'En en esta charla vamos a aprender como los conocimientos de front-end se pueden fusionar en el back-end',
-                imagem: '../../../../assets/img/maleja-giraldo.jpg'
-            },
-            {
-                dia: 'Jueves',
-                hora: '07:00pm - 7:30pm',
-                nome: 'Nicolas Molina Monroy',
-                titulo: 'Acelera la velocidad de navegación de tu Aplicación Angular',
-                desc: 'En esta charla vas a aprender a como acelerar la experiencia de navegación de tus usuarios en aplicaciones Angular.',
-                imagem: '../../../../assets/img/nicolas-monroy.jpg'
-            },
-            {
-                dia: 'Jueves',
-                hora: '07:30pm - 8:00pm',
-                nome: 'Sergio Valencia',
-                titulo: 'Performance en JS',
-                desc: 'Aprende sobre buenas prácticas que te ayudará a mejorar el performance de tu codigo',
-                imagem: '../../../../assets/img/sergio-valencia.jpg'
-            },
-            {
-                dia: 'Jueves',
-                hora: '08:00pm - 8:30pm',
-                nome: 'Fabio Cortés',
-                titulo: 'WebXR: La nueva generación de experiencias inmersivas en la web',
-                desc: 'Cuando hablamos de experiencias inmerivas en la web nos referimos a todo el espectro de aplicaciones que van desde el mundo real hasta la realidad virtual',
-                imagem: '../../../../assets/img/fabio-cortes.jpg'
-            },
-            {
-                dia: 'Jueves',
-                hora: '08:30pm - 9:00pm',
-                nome: 'Samuel Burbano',
-                titulo: 'Vue 3.0 - Composition API',
-                desc: 'Veamos la nueva Composition API propuesta para la próxima versión de Vue, como ayuda a mejorar el desarrollo en Vue y la polémica que ha generado en los desarrolladores que usan y aman Vue',
-                imagem: '../../../../assets/img/samuel-burbano.jpg'
-            },
-            {
-                dia: 'Miercoles',
-                hora: '09:00pm - 9:30pm',
-                nome: 'Mayra Rodriguez',
-                titulo: 'Promesas vs Observable',
-                desc: '¿Cuándo usar promesas? ¿Cuándo usar Observables? haremos una comparación entre ambos y daremos ejemplos reales de cuando hacer desarrollos imperativos o reactivos para que subas tu nivel',
-                imagem: '../../../../assets/img/mayra-rodriguez.png'
-            },
-            {
-                dia: 'Viernes',
-                hora: '07:00 pm - 7:00pm',
-                nome: 'Sebastian Gomez',
-                titulo: 'Manejando queues con Bull.js',
-                desc: 'Aprende a manejar facilmente queues y concurrencia con bull.js',
-                imagem: '../../../assets/img/sebastian.jpg'
-            },
-            {
-                dia: 'Viernes',
-                hora: '07:30pm - 8:00pm',
-                nome: 'Ricardo Chavarria',
-                titulo: 'WebPack, ¿Cómo aprovecharlo al máximo?',
-                desc: 'En está pequeña charla daremos los primeros pasos con Webpack para conocer sus beneficios y como aprovechalos al máximo, veremos cual es la necesidad de usarlo y tocaremos temas como Hot Reload, Minimización automática, Babel y otros temas interesantes de Webpack.',
-                imagem: '../../../assets/img/ricardo-chavarria.jpg'
-            },
-            {
-                dia: 'Viernes',
-                hora: '08:00pm - 8:30pm',
-                nome: 'Paula Mazo',
-                titulo: 'Docker para Front-ends',
-                desc: 'Que cosas debes tener en cuenta para usar Docker con tu proyecto en JS',
-                imagem: '../../../assets/img/paula-mazo.jpg'
-            },
-            {
-                dia     : 'Viernes',
-                hora    : '8:30pm - 9:00pm',
-                nome    : 'Vanessa Aristizabal',
-                titulo  : 'Meeseeks de JavaScript',
-                desc    : 'Los Meeseeks , son criaturas que sirven a un propósito en particular y hay algunas características de JavaScript que podríamos llamar Meeseeks, que nos facilitan mucho la creación de nuestros código y en esta charla quiero darles a conocer algunos de ellos.',
-                imagem: '../../../assets/img/vanessa.jpg'
-            },
-            {
-                dia: 'Viernes',
-                hora: '09:00pm - 9:30pm',
-                nome: 'Jesús Salvador Rodríguez Cardiel',
-                titulo: 'JS ha cambiado mi vida',
-                desc: 'Como JS revolucionó mi forma de trabajo',
-                imagem: '../../../assets/img/jesus-salvador.jpg'
-            }
-        ];
+        this.agendas = configAgenda;
     }
 
 }
